cae498217f4dde67fff8a822ce789f7a31d29c04
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2021-03-02 02:56:09 +0000

    Minor check added.

--- lib/AudioFile/Info/MP3/ID3Lib.pm
+++ lib/AudioFile/Info/MP3/ID3Lib.pm
@@ -19,6 +19,8 @@ my %data = (artist => 'TPE1',
 sub new {
   my $class = shift;
   my $file = shift;
+
+  die "Missing mp3 file." unless defined $file;
   my $obj = MP3::ID3Lib->new($file);
 
   bless { obj => $obj }, $class;
